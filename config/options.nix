3937a6e4dc697255689a028c2b6d3d2b49b57996
Dmutro4347 <dmutrobui@gmail.com>
2025-09-11 12:21:56 +0300

    make changes

--- config/options.nix
+++ config/options.nix
@@ -2,29 +2,27 @@
   config = {
     opts = {
       # === General Editor Settings ===
-      updatetime = 100;           # Time in ms to trigger CursorHold events
-      number = true;              # Show absolute line numbers
-#      relativenumber = true;      # Show relative line numbers
-      scrolloff = 7;              # Keep 7 lines visible above/below cursor
-      wrap = false;               # Disable line wrapping
-      cursorline = true;          # Highlight the current line
-      clipboard = "unnamedplus";  # Use system clipboard
-      termguicolors = true;       # Enable true color support
-      swapfile = false;           # Disable swapfile creation
+      updatetime = 100; # Time in ms to trigger CursorHold events
+      number = true; # Show absolute line numbers
+      #      relativenumber = true;      # Show relative line numbers
+      scrolloff = 7; # Keep 7 lines visible above/below cursor
+      wrap = false; # Disable line wrapping
+      cursorline = true; # Highlight the current line
+      clipboard = "unnamedplus"; # Use system clipboard
+      termguicolors = true; # Enable true color support
+      swapfile = false; # Disable swapfile creation
 
       # === Indentation ===
-      autoindent = true;          # Copy indent from current line
-      smartindent = true;         # Smart auto-indenting for C-like programs
+      autoindent = true; # Copy indent from current line
+      smartindent = true; # Smart auto-indenting for C-like programs
       smarttab = true;
-      expandtab = true;           # Convert tabs to spaces
-      shiftwidth = 2;             # Number of spaces for each step of autoindent
-      tabstop = 2;                # Number of spaces that a <Tab> counts for
+      expandtab = true; # Convert tabs to spaces
+      shiftwidth = 2; # Number of spaces for each step of autoindent
+      tabstop = 2; # Number of spaces that a <Tab> counts for
 
       # === Mouse ===
-      mouse = "a";                # Enable mouse support in all modes
+      mouse = "a"; # Enable mouse support in all modes
+      guifont = "Monocraft Nerd Font:h14";
     };
   };
 }
-
-
-
